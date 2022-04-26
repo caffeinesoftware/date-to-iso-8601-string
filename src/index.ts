b7630ac568c4bcc7f18c18a074ae303fc8dca81e
Tim Rogers <me@timrogers.co.uk>
2022-04-26 14:19:26 +0100

    Present the local date of the Date object, not the UTC date

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,39 @@ export enum ISO8601DateFormat {
 }
 
 /**
- * Presents the supplied JavaScript `Date` as a string in ISO 8601 format,
- * defaulting to the Extended date format (`YYYY-MM-DD`).
+ * Presents the supplied JavaScript `Date` as a string in ISO 8601 Basic format,
+ * `YYYYMMDD` (e.g. `20230101`) (see
+ * <https://www.loc.gov/standards/datetime/iso-tc154-wg5_n0038_iso_wd_8601-1_2016-02-16.pdf>).
+ * 
+ * @param date the date to present
+ */
+const dateToISO8601BasicFormatString = (date: Date): string => {
+  const yyyy = date.getFullYear().toString();
+  const mm = (date.getMonth() + 1).toString().padStart(2, '0');
+  const dd = date.getDate().toString().padStart(2, '0');
+
+  return [yyyy, mm, dd].join('');
+}
+
+/**
+ * Presents the supplied JavaScript `Date` as a string in ISO 8601 Extended format,
+ * `YYYY-MM-DD` (e.g. `2023-01-01`) (see
+ * <https://www.loc.gov/standards/datetime/iso-tc154-wg5_n0038_iso_wd_8601-1_2016-02-16.pdf>).
+ * 
+ * @param date the date to present
+ */
+const dateToISO8601ExtendedFormatString = (date: Date): string => { 
+  const yyyy = date.getFullYear().toString();
+  const mm = (date.getMonth() + 1).toString().padStart(2, '0');
+  const dd = date.getDate().toString().padStart(2, '0');
+
+  return [yyyy, mm, dd].join('-');
+}
+
+/**
+ * Presents the supplied JavaScript `Date` as a date string in ISO 8601 format,
+ * using the local time, rather than UTC. Defaults to the Extended date format
+ * (`YYYY-MM-DD`).
  *
  * @param date the date to present
  * @param format the ISO 8601 format to use for presentation, defaulting to
@@ -23,8 +54,8 @@ export const dateToISO8601String = (
 ): string => {
   switch (format) {
     case ISO8601DateFormat.Basic:
-      return date.toISOString().split('T')[0].replace(/-/g, '');
+      return dateToISO8601BasicFormatString(date);
     case ISO8601DateFormat.Extended:
-      return date.toISOString().split('T')[0];
+      return dateToISO8601ExtendedFormatString(date);
   }
 };
